d5064a2d3908fb6055cb9ba4e33052c6d2334711
juliovianadev <juliovianadev@gmail.com>
2023-04-15 10:13:26 -0300

    função que retorna duas coisas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import FunctionSimples from './exemplos/components/FunctionSimples';
+import FunctionDupla from './exemplos/components/FunctionDupla';
 
 function App() {
   return (
     <div className="App">
       {/* chamando um componente simples */}
       <FunctionSimples/>
+      {/* chamando a função dupla */}
+      <FunctionDupla/>
     </div>
   );
 }
