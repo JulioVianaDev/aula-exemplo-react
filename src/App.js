0b1680f2f7bc8684acd79929af4d8dc0de6b7d96
juliovianadev <juliovianadev@gmail.com>
2023-04-15 10:38:40 -0300

    explicação useState

--- src/App.js
+++ src/App.js
@@ -2,8 +2,15 @@ import './App.css';
 import FunctionSimples from './exemplos/components/FunctionSimples';
 import FunctionDupla from './exemplos/components/FunctionDupla';
 import Botao from './exemplos/layout/Botao';
-
+import { useState } from 'react';
 function App() {
+  // o useState tem como parametro 3 coisas
+  // a primeira é uma variavel aonde será armazenada o estado
+  // o segundo é a função que vai mudar o valor da variavel
+  // e dentro do useState você pode por o valor inicial, seja booleano,string,objeto,array,lista,etc
+  // lembre-se, só se pode mudar o valor da variavel pela função
+  const [variavel,functionParaMudarValorDaVariavel]=useState("valor inicial")
+  
   return (
     <div className="App">
       {/* chamando um componente simples */}
